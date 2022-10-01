a4781f5eb00c1260f12330aa14ad88e5cbef8785
rbelounis-phenryrlabs <rbelounis@whitelabgx.com>
2022-10-01 02:08:53 +0200

    :speech_balloon: update for text test
    Small update for bolerplate text before real development begins

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hey this is our new website for <a href="phenryrlabs.com">phenrylabs</a>, welcome !
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <small>(And you can see this is the boilerplate for React)</small>
       </header>
     </div>
   );
